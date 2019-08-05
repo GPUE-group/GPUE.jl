2ec26c49f968cd1e8c63169e8d86b8761fdab865
Benjamin Chislett <chislett.ben@gmail.com>
2019-08-05 17:28:14 +0900

    Add shell for hdf5 output

---
+++ src/io/output.jl
@@ -0,0 +1,33 @@
+## Creating new output
+
+function writeDataSet()
+
+end
+
+function writeAttribute()
+
+end
+
+function init()
+
+end
+
+
+## Loading previous output
+
+function loadGroup()
+
+end
+
+function loadAttribute()
+
+end
+
+function loadDataSet()
+
+end
+
+function load()
+
+end
+
