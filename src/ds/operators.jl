ee5765cc28ddc23700286c693aa12be102a06ed0
benchislett <chislett.ben@gmail.com>
2019-08-14 11:38:57 +0900

    Add correct imaginary components to operators

--- src/ds/operators.jl
+++ src/ds/operators.jl
@@ -15,16 +15,16 @@ function Operators(par::Params)
   Vz = @. par.omegaZ * par.z
 
   V = @. 0.5 * par.mass * (Vx * Vx + Vy * Vy + Vz * Vz)
-  V = @. exp(-0.5 * par.dt / ħ * V)
+  V = @. exp(-0.5im * par.dt / ħ * V)
 
-  K = @. exp(-par.dt / ħ * par.k)
+  K = @. exp(-1.0im * par.dt / ħ * par.k)
 
   ϕ = @. (par.winding * atan(par.y, par.x)) % (2π)
 
   wfc = @. exp(-((par.x / par.Rxy / par.a0x) * (par.x / par.Rxy / par.a0x)
                 +(par.y / par.Rxy / par.a0y) * (par.y / par.Rxy / par.a0y)
-                +(par.z / par.Rxy / par.a0z) * (par.z / par.Rxy / par.a0z))
-                + ϕ * im)
+                +(par.z / par.Rxy / par.a0z) * (par.z / par.Rxy / par.a0z)))
+  wfc = @. wfc * cos(ϕ) + wfc * im * sin(ϕ)
 
   Ax = CuArray{Complex{Float64}}(undef, size(wfc))
   Ay = CuArray{Complex{Float64}}(undef, size(wfc))
@@ -34,9 +34,9 @@ function Operators(par::Params)
   @. Ay = -par.x * par.omega * par.omegaZ
   @. Az = 0
 
-  Ax = @. exp(Ax * par.px * par.dt)
-  Ay = @. exp(Ay * par.py * par.dt)
-  Az = @. exp(Az * par.pz * par.dt)
+  Ax = @. exp(-1.0im * Ax * par.px * par.dt)
+  Ay = @. exp(-1.0im * Ay * par.py * par.dt)
+  Az = @. exp(-1.0im * Az * par.pz * par.dt)
 
   return Operators(V, K, wfc, Ax, Ay, Az)
 end
