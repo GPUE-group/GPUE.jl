8731b6d908b418a19180a4de5bcb587f2d004186
benchislett <chislett.ben@gmail.com>
2019-08-16 17:32:51 +0900

    Add annotation to the plot output
    Also clamps the range a bit tighter, which will be subject to change

--- src/utils/plot.jl
+++ src/utils/plot.jl
@@ -10,9 +10,11 @@ gstate = read(attrs(f.file)["gstate"])
 group = gstate ? f.wfc_const : f.wfc_ev
 
 anim = @animate for name in names(group)
+  println("Writing frame: $name")
   dset = group[name]
   data = read(dset)
-  heatmap(abs.(data), clims=(0, 2e5))
+  heatmap(abs.(data), clims=(0, 4e4))
+  annotate!([(0, -20, "$name")])
 end
 
 gif(anim, "./output/wfc.gif", fps=5)
