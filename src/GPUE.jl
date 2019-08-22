0bf97d409957463db07b7ef0e74564262dae89cc
benchislett <chislett.ben@gmail.com>
2019-08-22 18:12:12 +0900

    Add documentation for main GPUE module

--- src/GPUE.jl
+++ src/GPUE.jl
@@ -1,16 +1,34 @@
+"""
+    GPUE.jl
+
+Gross—Pitaevskii equation solver that is accelerated on GPU hardware with CUDA.
+
+GPUE.jl has been developed as a maintainable alternative to [GPUE](https://github.com/GPUE-group/GPUE),
+which functions similarly and is written in CUDA/C++.
+For more on the original GPUE, see the [documentation](https://github.com/GPUE-group/GPUE)
+"""
 module GPUE
-  using CuArrays, CUDAnative, FFTW
+  # Required GPU libraries
+  using CuArrays, CUDAnative
+
+  # Import FFT definitions from CuArrays through AbstractFFTs
+  using AbstractFFTs
+
+  # Import I/O functionality through HDF5.jl
   using HDF5
 
+  # Mathematical and Physical Constants
   const ħ = 1.0545718176461565e-34
 
   
-  # Temporary hack until CUDAnative issue #430 gets fixed
+  # Implement GPU wrappers for operations on complex numbers
+  # Can be removed when CUDAnative issue #430 gets fixed
   @inline CUDAnative.exp(x::Complex{Float64}) = CUDAnative.exp(x.re) * (CUDAnative.cos(x.im) + 1.0im * CUDAnative.sin(x.im))
   @inline CUDAnative.abs(x::Complex{Float64}) = CUDAnative.hypot(x.re, x.im)
   @inline CUDAnative.abs2(x::Complex{Float64}) = x.re * x.re + x.im * x.im
 
-  # Temporary hack until CUDAnative PR #443 gets merged
+  # Rename atan2 to atan as per julia standard,
+  # Can be removed when CUDAnative PR #443 gets merged
   @inline CUDAnative.atan(y, x) = CUDAnative.atan2(y, x)
 
   # Load data structures
