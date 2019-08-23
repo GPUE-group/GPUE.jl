bbe3dcdf724e5eb05f2b03783f87a4f9222fad1b
benchislett <chislett.ben@gmail.com>
2019-08-23 14:58:25 +0900

    Refactor main.jl into an IIF

--- src/main.jl
+++ src/main.jl
@@ -1,13 +1,17 @@
 using GPUE
 
+function main()
+  f = initFileData()
 
-f = initFileData()
-par = Params()
-opr = Operators(par)
-aux = Aux(par, opr)
+  par = Params(writeOut=false, xDim=512, yDim=512, printSteps=2500, iterations=1, dt=1e-4im, omega=0.6, nAtoms=1e5, omegaZ=100)
+  opr = Operators(par)
+  aux = Aux(par, opr)
 
-try
-  evolve(f, par, opr, aux)
-finally
-  terminate(f, par, opr, aux)
+  try
+    evolve!(f, par, opr, aux)
+  finally
+    terminate(f, par, opr, aux)
+  end
 end
+
+main()
